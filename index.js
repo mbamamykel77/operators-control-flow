26963c745a8c00d7bda3eaa9f9202ca79cca1c4e
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-02-23 13:57:15 +0100

    code modification

--- index.js
+++ index.js
@@ -8,20 +8,24 @@
 // Otherwise: General Subject
 
 
-let classGroup = 'social science';
+let classGroup = 'arts';
+let artSubject = 'English, Mathematics, Government, Economics, Literature, History'
+let scienceSubject = 'English, Mathematics, Physics, Chemistry, Biology, Technical Drawing'
+let socialScienceSubject = 'English, Mathematics, Accounting, Commerce, Marketing, Geography'
+let generalSubject = 'English, Mathematics'
 
 
-if (classGroup == 'Arts') {
-    console.log('English, Mathematics, Government, Economics, Literature, History');
+if (classGroup == 'arts') {
+    console.log(artSubject);
 } 
 else if (classGroup == 'science') {
-    console.log('English, Mathematics, Physics, Chemistry, Biology, Technical Drawing');
+    console.log(scienceSubject);
 } 
 else if (classGroup == 'social science') {
-    console.log('English, Mathematics, Accounting, Commerce, Marketing, Geography');
+    console.log(socialScienceSubject);
 } 
 else {
-    console.log('English, Mathematics')
+    console.log(generalSubject)
 }
 
 // Answer: English, Mathematics, Government, Economics, Literature, History
@@ -31,25 +35,25 @@ else {
 
 // Question 5
 
-let pwr; 
-let firstValue; 
-const nearestPower = (num) => {
-  for (let i = 0; i < num; i++) {
-    pwr = 2 ** i;
-    if (pwr < num) {
-      firstValue = pwr;
-    }
-  }
-  if (num - firstValue > firstValue * 2 - num) {
-    pwr = firstValue * 2
-  }
-  else if (num - firstValue < firstValue * 2 - num) {
-    pwr = firstValue 
-  }
-  else {return "not a valid number"}
-  return `The number ${pwr} is the power of 2 nearest to ${num}`;
-};
-console.log(nearestPower(20));
+// let pwr; 
+// let firstValue; 
+// const nearestPower = (num) => {
+//   for (let i = 0; i < num; i++) {
+//     pwr = 2 ** i;
+//     if (pwr < num) {
+//       firstValue = pwr;
+//     }
+//   }
+//   if (num - firstValue > firstValue * 2 - num) {
+//     pwr = firstValue * 2
+//   }
+//   else if (num - firstValue < firstValue * 2 - num) {
+//     pwr = firstValue 
+//   }
+//   else {return "not a valid number"}
+//   return `The number ${pwr} is the power of 2 nearest to ${num}`;
+// };
+// console.log(nearestPower(20));
 
 // Answer: The number 16 is the power of 2 nearest to 20
  
